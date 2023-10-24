0e9c7c941b84ac96960d6b31ab74167758ea5c24
Asel <mirbekovaasel663@gmail.com>
2023-10-24 17:06:03 +0600

    Initial Commit

---
+++ Neobis_iOS_StopWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_StopWatch
+//
+//  Created by Asel Mirbek on 24/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
